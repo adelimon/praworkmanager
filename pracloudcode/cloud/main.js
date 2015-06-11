a1602ae7da19887406b9ea247b4339d7b1f9383e
Alan Delimon <adelimon@gmail.com>
2015-06-11 15:44:24 +0000

    updated cloud code to get email working again, added cloud code to git.

---
+++ pracloudcode/cloud/main.js
@@ -0,0 +1,40 @@
+Parse.Cloud.define("sendJobNotification",
+    function(request, response) {
+        var Mailgun = require("mailgun");
+        Mailgun.initialize('sandboxd12b57cf73ce4b56ad3a175e89c5c8e1.mailgun.org', 'key-6d3648873bf6a19a939569cdda457c66');
+        var msgSubject = request.params.name + " signed up for " + request.params.job + " on " + request.params.event;
+        Mailgun.sendEmail({
+            to: "adelimon@gmail.com, hogbacksecretary@gmail.com, dlstone727@yahoo.com",
+            from: "signup@palmyramx.com",
+            subject: msgSubject,
+            text: msgSubject + ". Please see the signup sheet for details."
+        }, {
+            success: function(httpResponse) {
+                console.log(httpResponse);
+                response.success("Email sent!");
+            },
+            error: function(httpResponse) {
+                console.error(httpResponse);
+                response.error("Uh oh, something went wrong");
+            }
+        });
+    }
+);
+
+Parse.Cloud.define("listEvents",
+    function(request, response) {
+        var eventQuery = new Parse.Query("ScheduleDates");
+        eventQuery.ascending("eventDate");
+        eventQuery.find({
+            success: function(results) {
+                response.success(results);
+            }
+        });
+    }
+);
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+    response.success("Hello world!");
+});
