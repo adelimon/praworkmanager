919f2a1cca495323f92cea4880dc80622a70a796
Alan Delimon <adelimon@gmail.com>
2015-06-12 17:46:48 +0000

    update signup js to v3.1

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Starting deploy...."
+echo "Committing to github...."
 git add .
 git commit -m "$1"
 git push -u origin master
-git ftp push
+#echo "ftp push to bluehost...."
+#git ftp push
+echo "pushing cloud code to parse...."
 cd pracloudcode
 parse deploy
 cd
