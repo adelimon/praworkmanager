49e4c0f4d1a5178ce95bc81860e5d42c0405300b
Alan Delimon <adelimon@gmail.com>
2015-06-12 17:34:07 +0000

    update with latest file changes

--- pracloudcode/cloud/main.js
+++ pracloudcode/cloud/main.js
@@ -1,28 +1,7 @@
-Parse.Cloud.define("sendJobNotification",
-    function(request, response) {
-        /*
-        var Mailgun = require("mailgun");
-        Mailgun.initialize('sandboxd12b57cf73ce4b56ad3a175e89c5c8e1.mailgun.org', 'key-6d3648873bf6a19a939569cdda457c66');
-        var msgSubject = request.params.name + " signed up for " + request.params.job + " on " + request.params.event;
-        Mailgun.sendEmail({
-            to: "adelimon@gmail.com, hogbacksecretary@gmail.com, dlstone727@yahoo.com",
-            from: "signup@palmyramx.com",
-            subject: msgSubject,
-            text: msgSubject + ". Please see the signup sheet for details."
-        }, {
-            success: function(httpResponse) {
-                console.log(httpResponse);
-                response.success("Email sent!");
-            },
-            error: function(httpResponse) {
-                console.error(httpResponse);
-                response.error("Uh oh, something went wrong");
-            }
-        });
-        */
-    }
-);
-
+/**
+ * Trigger after a signup is entered - send an email.  Do this in cloud code
+ * rather than on the client, because that's where it belongs.
+  */
 Parse.Cloud.afterSave("Signup",
     function(request) {
         var name = request.object.get("name");
@@ -52,6 +31,10 @@ Parse.Cloud.afterSave("Signup",
     }
 );
 
+/**
+ * List the events associated with a given calendar year.
+ * @return a list of the results, sorted in ascending order.
+ */
 Parse.Cloud.define("listEvents",
     function(request, response) {
         var eventQuery = new Parse.Query("ScheduleDates");
